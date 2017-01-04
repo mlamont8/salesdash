9e23aad210ea967fef8947dd2e23301e5fa09b80
mlamont8 <debiandmiat@hotmail.com>
2017-01-04 14:59:02 -0500

    add sales table

---
+++ database/migrations/2017_01_04_184550_create_sales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('company');
+            $table->decimal('value',9,2);
+            $table->decimal('commission',9,2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
